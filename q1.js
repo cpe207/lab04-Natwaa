82c61ab5a273adff2c01b1ffad3630cfbb576c64
Natwaa <Natwara_ch@cmu.ac.th>
2024-07-02 16:47:30 +0700

    finishhhhhhhh

--- q1.js
+++ q1.js
@@ -1,19 +1,16 @@
 function typeChecker(a, b) {
-  return typeof a === typeof b;
+    return typeof a === typeof b;
 }
-
 //Test cases
-const a1 = "Jack";
-const b1 = "Rose";
-const a2 = 555;
-const b2 = 100;
-const a3 = "Jack";
-const b3 = 100;
-
+var a1 = "Jack";
+var b1 = "Rose";
+var a2 = 555;
+var b2 = 100;
+var a3 = "Jack";
+var b3 = 100;
 console.log(typeChecker(a1, b1));
 console.log(typeChecker(a2, b2));
 console.log(typeChecker(a3, b3));
-
 module.exports = typeChecker;
 
 //ณัฐวรา ไชยสิทธิ์
